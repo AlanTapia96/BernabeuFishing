01bb0cb2007bcadfe40dcdd003619171e14d8a79
alan.tapia1 <alan.tapia@e-cruce.com>
2021-11-27 14:32:27 -0300

    sticky navbar + ajustes modal responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,25 @@
 import scrollTop from "./scroll_top.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  scrollTop(".scroll-top-btn");
 
-    scrollTop(".scroll-top-btn");
+  document.addEventListener("click", (e) => {
+    /* language change */
+    if (e.target.matches(".lang")) {
+      const esp = document.querySelector("#esp");
+      const china = document.querySelector("#china");
+      if (e.target.matches("#esp")) {
+        esp.classList.add("active");
+        china.classList.remove("active");
+      } else {
+        china.classList.add("active");
+        esp.classList.remove("active");
+      }
+    }
+  });
 
-    document.addEventListener('click', (e) => {
-
-        /* language change */
-        if(e.target.matches('.lang')){
-            const esp = document.querySelector('#esp');
-            const china = document.querySelector('#china')
-            if(e.target.matches('#esp')){
-                esp.classList.add('active')
-                china.classList.remove('active')
-            } else {
-                china.classList.add('active')
-                esp.classList.remove('active')
-            }
-        }
-
-        
-
-    })
-    
-
-
-
-})
+  window.addEventListener("scroll", () => {
+    let header = document.querySelector("header nav");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+});
