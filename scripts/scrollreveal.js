3ee72f5b659e3b87f263f12b752cd87d7e82aaae
miguelescalera <tapia.alan.g@gmail.com>
2021-12-16 12:54:28 -0300

    fix scrollReveal products

--- scripts/scrollreveal.js
+++ scripts/scrollreveal.js
@@ -1,38 +1,41 @@
 window.sr = ScrollReveal();
 
-sr.reveal(".principal-h1", {
-  duration: 2000,
-  origin: "top",
-  distance: "200px",
-});
+const accordionButton = document.querySelector(".accordion-button");
+accordionButton.onclick = (e) => {
+  console.log(e);
+  sr.reveal(".principal-h1", {
+    duration: 2000,
+    origin: "top",
+    distance: "200px",
+  });
 
-sr.reveal(".principal-p", {
-  duration: 2000,
-  origin: "top",
-  distance: "200px",
-});
+  sr.reveal(".principal-p", {
+    duration: 2000,
+    origin: "top",
+    distance: "200px",
+  });
 
-sr.reveal(".principal-img", {
-  duration: 2000,
-  origin: "bottom",
-  distance: "300px",
-});
+  sr.reveal(".principal-img", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "300px",
+  });
 
-sr.reveal('.accordion-item h3', {
-  delay: 200,
-  duration: 450,
-  reset: false
-});
+  sr.reveal(".accordion-item h3", {
+    delay: 200,
+    duration: 450,
+    reset: false,
+  });
 
-sr.reveal('.accordion-item .caracteristicas', {
-  delay: 200,
-  duration: 450,
-  reset: false
-});
+  sr.reveal(".accordion-item .caracteristicas", {
+    delay: 200,
+    duration: 450,
+    reset: false,
+  });
 
-sr.reveal('.card', {
-  delay: 300,
-  duration: 450,
-  reset: false
-});
-
+  sr.reveal(".card", {
+    delay: 300,
+    duration: 450,
+    reset: false,
+  });
+};
